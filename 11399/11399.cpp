7914e6da13f26bfe3c08aae223bfd5aecc4f68d6
Park52 <wogus8660@gmail.com>
2019-06-14 21:27:05 +0900

    11399

---
+++ 11399/11399.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int time[1001] = { 0 };
+	int n;
+	int result = 0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> time[i];
+	}
+
+
+	sort(time+1, time + n +1);
+	
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			result += time[j];
+		}
+	}
+	
+	cout << result;
+	
+	return 0;
+}

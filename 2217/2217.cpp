195588008fc22cf3101224368bf03d5a358e301a
Park52 <wogus8660@gmail.com>
2019-06-16 04:24:20 +0900

    2217

---
+++ 2217/2217.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[100001];
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	sort(a , a + n );
+
+	int max = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (max < a[i] * (n - i))
+		{
+			max = a[i] * (n - i);
+		}
+	}
+
+	cout << max;
+	return 0;
+}

6c724c98d994d33bc5bb2680e8e8a1b0107bd929
Park52 <wogus8660@gmail.com>
2019-06-20 21:45:59 +0900

    1138

---
+++ 1138/1138.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	int people[11] = { 0, };
+	int res[11] = { 0, };
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> people[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
+
+	return 0;
+}

0e21a212db84b63ef70ae252ae634dea9025f2ce
Park52 <wogus8660@gmail.com>
2019-07-04 20:19:47 +0900

    10845

---
+++ 10845/10845.cpp
@@ -0,0 +1,80 @@
+﻿#include <queue>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+queue<int> q;
+
+int main(void)
+{
+	cin >> N;
+	int Num;
+	for (int i = 0; i < N; i++)
+	{
+		//getline(cin, str);
+		string str;
+		cin >> str;
+
+		if (str == "push")
+		{
+			int nData;
+			cin >> nData;
+			q.push(nData);
+		}
+		else if (str == "pop")
+		{
+			if (!q.empty())
+			{
+				Num = q.front();
+				q.pop();
+			}
+			else
+			{
+				Num = -1;
+			}
+			cout << Num << endl;
+		}
+		else if (str == "size")
+		{
+
+			cout << q.size() << endl;
+
+		}
+		else if (str == "empty")
+		{
+			if (q.size() == 0) Num = 1;
+			else Num = 0;
+			cout << Num << endl;
+		}
+		else if (str == "front")
+		{
+			if (q.size() == 0)
+			{
+				Num = -1;
+			}
+			else
+			{
+				Num = q.front();
+			}
+			cout << Num << endl;
+		}
+		else if (str == "back")
+		{
+			if (q.size() == 0)
+			{
+				Num = -1;
+			}
+			else
+			{
+				Num = q.back();
+			}
+			cout << Num << endl;
+		}
+
+	}
+
+
+	return 0;
+}

67037ccd1fcef65354e13b864a31c43f7b109c9e
Park52 <wogus8660@gmail.com>
2019-07-04 22:17:13 +0900

    10828

---
+++ 10828/10828.cpp
@@ -0,0 +1,68 @@
+﻿#include <stack>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+stack<int> s;
+
+int main(void)
+{
+	cin >> N;
+	int Num;
+	for (int i = 0; i < N; i++)
+	{
+		//getline(cin, str);
+		string str;
+		cin >> str;
+
+		if (str == "push")
+		{
+			int nData;
+			cin >> nData;
+			s.push(nData);
+		}
+		else if (str == "pop")
+		{
+			if (!s.empty())
+			{
+				Num = s.top();
+				s.pop();
+			}
+			else
+			{
+				Num = -1;
+			}
+			cout << Num << endl;
+		}
+		else if (str == "size")
+		{
+
+			cout << s.size() << endl;
+
+		}
+		else if (str == "empty")
+		{
+			if (s.size() == 0) Num = 1;
+			else Num = 0;
+			cout << Num << endl;
+		}
+		else if (str == "top")
+		{
+			if (s.size() == 0)
+			{
+				Num = -1;
+			}
+			else
+			{
+				Num = s.top();
+			}
+			cout << Num << endl;
+		}
+
+	}
+
+
+	return 0;
+}

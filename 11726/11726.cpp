efc19fcead73c8a1bd000b510e581428c1b64227
Park52 <wogus8660@gmail.com>
2019-07-11 14:16:39 +0900

    11726
    Dynamic Programming

---
+++ 11726/11726.cpp
@@ -0,0 +1,31 @@
+﻿
+/**********
+1. 점화식
+
+D[n] = D[n-1] + D[n-2];
+
+**********/
+
+#include <iostream>
+
+using namespace std;
+
+int d[1001];
+
+int dp(int x)
+{
+	if (x == 1) return 1;
+	if (x == 2) return 2;
+
+	if (d[x] != 0) return d[x];
+	return d[x] = (dp(x - 1) + dp(x - 2)) % 10007;
+}
+
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	cout << dp(N) << endl;
+	return 0;
+}

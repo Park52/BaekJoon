245e0041133958de59c5f8dcc4a0d951814fd3e6
Park52 <wogus8660@gmail.com>
2019-11-18 23:45:43 +0900

    10814

---
+++ 10814/10814.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+typedef struct stInfo {
+	int age;
+	string name;
+}INFO;
+
+bool cmp(const INFO& A, const INFO& B)
+{
+	return A.age < B.age;
+}
+
+int main(void)
+{
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	INFO temp;
+
+	cin >> N;
+
+	vector<INFO> v(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i].age >> v[i].name;
+	}
+
+	stable_sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << v[i].age << ' ' << v[i].name << '\n';
+	}
+	return 0;
+}

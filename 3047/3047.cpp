b92120c0d0007eadfe072b27d71ec25dabca914c
Park52 <wogus8660@gmail.com>
2019-12-10 03:13:37 +0900

    3047

---
+++ 3047/3047.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int arr[3];
+	char ABC[4];
+
+	for(int i = 0; i < 3; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	
+	for (int i = 0; i < 3; i++)
+	{
+		scanf(" %c", &ABC[i]);
+	}
+
+	sort(arr, arr + 3);
+
+	for (int i = 0; i < 3; i++)
+	{
+		switch (ABC[i])
+		{
+		case 'A':
+			printf("%d ", arr[0]);
+			break;
+		case 'B':
+			printf("%d ", arr[1]);
+			break;
+		case 'C':
+			printf("%d ", arr[2]);
+			break;
+		}
+	}
+
+	printf("\n");
+	return 0;
+}
